3f826e313c85cfb57fd9bdb6d8ad043fb560e73d
Hughuez <hughuez.and@gmail.com>
2022-08-18 17:06:38 -0500

    añadiendo cambios clase 2

--- src/main/java/com/minTicUdea/model/Order.java
+++ src/main/java/com/minTicUdea/model/Order.java
@@ -1,5 +1,7 @@
 package com.minTicUdea.model;
 
+import com.minTicUdea.model.enums.OrderStatus;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,5 +11,62 @@ public class Order {
     private LocalDate orderDate;
     private Client client;
     private List<OrderItem> items = new ArrayList<>();
+    private OrderStatus status;
+
+    public Order(LocalDate orderDate, Client client, List<OrderItem> items, OrderStatus status) {
+        this.orderDate = orderDate;
+        this.client = client;
+        this.items = items;
+        this.status = status;
+    }
+
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void addItem (OrderItem item){
+        items.add(item);
+    }
+
+    public void removeItem (OrderItem item){
+        items.remove(item);
+    }
+
+    public double total(){
+        double sum = 0.0;
+            for(OrderItem it: items){
+                sum += it.subTotal();
+            }
+        return sum;
+    }
 
 }
---
+++ src/main/java/com/minTicUdea/model/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.minTicUdea.model.enums;
+
+public enum OrderStatus {
+    /*se crean las constantes, que no van a cambiar*/
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED
+}
